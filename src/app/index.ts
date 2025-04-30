16c6b4bf82676a889309f36bd707e861e86e2c74
Vik63 <sfoto116@yandex.ru>
2025-04-30 19:07:27 +0400

    edit catalog1

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,19 +3,28 @@ import './styles/index.css';
 import lozad from '@/utils/lib/lozad/lozad';
 import { slider } from '@/blocks/slider/slider';
 import { handleOwn } from '@/blocks/own/own';
-console.log('g:', window.location.href);
+const arr = window.location.href.split('/');
+const location = arr[arr.length - 1];
+console.log('location:', location);
 // === ленивая загрузка фотографий ==========
 const observerLazy = lozad();
 observerLazy.observe();
 
 // === анимация при скролле вправо ================================
 const elementsOnRight = document.querySelectorAll('.animation-right');
+const hommet = document.querySelector('.hommet');
+const marquee = document.querySelector('.marquee');
 elementsOnRight.forEach((el) => {
    animationScrolling(el, 'right');
 });
 
 // === слайдер =====
-slider();
+if (location !== 'catalog') {
+   slider();
+} else {
+   hommet?.classList.add('destroy');
+   marquee?.classList.add('destroy');
+}
 
 // === собственное производство =====
 
