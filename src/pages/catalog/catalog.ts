bfc6eac070cdf466cc9a7521f0b8a707395499ec
Vik63 <sfoto116@yandex.ru>
2025-05-02 16:19:36 +0400

    edit redirect_4

--- src/pages/catalog/catalog.ts
+++ src/pages/catalog/catalog.ts
@@ -3,6 +3,15 @@ import { changeUrl } from '@/utils/lib/changeUrl/changeUrl';
 console.log('a:', window.location.href);
 console.log('h:', window.history);
 
+if (location.pathname !== '/') {
+   console.log('location.pathname:', location.pathname);
+
+   const address = __IS_DEV__
+      ? `${location.pathname}.html`
+      : `https://academy-heat-hommet.vercel.app${location.pathname}.html`;
+   location.href = address;
+}
+
 changeUrl();
 
 console.log('b:', window.location.href);
