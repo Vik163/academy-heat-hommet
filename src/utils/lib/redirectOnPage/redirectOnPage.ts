d40e54d106b416b7daf6d710fecde0e7d3827956
Vik63 <sfoto116@yandex.ru>
2025-05-04 18:18:20 +0400

    edit redirect_20

--- src/utils/lib/redirectOnPage/redirectOnPage.ts
+++ src/utils/lib/redirectOnPage/redirectOnPage.ts
@@ -1,6 +1,6 @@
 export const redirectOnPage = (page: 'catalog' | 'conatacts', path: string) => {
    const address = __IS_DEV__
       ? `${page}.html`
-      : `https://academy-heat-hommet.vercel.app/${page}.html`;
+      : `https://academy-heat-hommet.vercel.app/${page}`;
    window.location.href = address;
 };
