791405c31553c4f591d49db3e375a70f7bd17d4a
Vik63 <sfoto116@yandex.ru>
2025-05-16 22:10:36 +0400

    add modal lightgallery

--- src/blocks/catalog/product-block/product-block.ts
+++ src/blocks/catalog/product-block/product-block.ts
@@ -1,4 +1,6 @@
 import type { Card } from '@/utils/types/cards';
+import lightGallery from 'lightgallery';
+import 'lightgallery/css/lightgallery.css';
 
 export const setProduct = (card: Card) => {
    const productBlock = document.querySelector('.product')!;
@@ -6,8 +8,22 @@ export const setProduct = (card: Card) => {
    const titleProduct = productBlock.querySelector('.product__title')!;
    const imageProduct = productBlock.querySelector(
       '.product__image',
-   )! as HTMLImageElement;
+   )! as HTMLLinkElement;
    titleProduct.textContent = card.title;
-   imageProduct.alt = card.title;
-   imageProduct.src = card.imgL!;
+   imageProduct.setAttribute('data-src', card.imgB!);
+
+   const img = document.querySelector('.modal__image') as HTMLImageElement;
+   img.src = card.imgL!;
+   img.alt = card.title;
+
+   lightGallery(document.getElementById('animated-thumbnails-gallery')!, {
+      // plugins: [lgThumbnail],
+      // addClass: 'lg-custom-thumbnails',
+      // // Remove the starting animations.
+      // // This can be done by overriding CSS as well
+      // appendThumbnailsTo: '.lg-outer',
+   });
+
+   const bg = document.querySelector('.lg-backdrop') as HTMLDivElement;
+   bg.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
 };
