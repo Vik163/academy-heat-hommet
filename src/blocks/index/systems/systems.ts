91e356481d724ff888c138988b64c4a99bebf335
Vik63 <sfoto116@yandex.ru>
2025-05-28 13:48:30 +0400

    fix bugs

--- src/blocks/index/systems/systems.ts
+++ src/blocks/index/systems/systems.ts
@@ -1,10 +1,12 @@
 import { redirectOnPage } from '@/utils/lib/redirectOnPage';
+import { setLocalStorage } from '@/utils/lib/setLocalStorage';
 
 const links = document.querySelectorAll('.systems__item-link');
 export const setSystems = () => {
    links.forEach((link) => {
       link.addEventListener('click', function (e: Event) {
          e.preventDefault();
+         setLocalStorage('', '', '');
          redirectOnPage('catalog');
       });
    });
